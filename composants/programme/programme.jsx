fcd051d3d23ee478eeeef1d9fffbaf66f1560e44
Daniel Seppo Eke <juniorseppo3@gmail.com>
2023-04-20 18:05:51 +0200

    ajout de programme sur la page d'accueil

--- composants/programme/programme.jsx
+++ composants/programme/programme.jsx
@@ -1,7 +1,42 @@
+import Container from "../container/container";
+import ProgrammeItems from "../programmeItems/programmeItem";
+import styles from './programme.module.css'
+
 export default function Programme(){
     return(
         <section id="programme">
-            
+            <CourbeIn class = {styles.courbe_in}/>
+            <Container>
+                <div class = {styles.programme}>
+                    <div class = {styles.first}>
+                        <div class='h2' style={{color : '#7b24ea', "text-align": 'center'}}>Foire aux Programmes</div>
+                        <div class="p" style={{ "text-align" : 'center'}}>Un nouveau mode de formation par Programmes permettant d’associer 
+                        l’enseignement général et l’enseignement professionnel et surtout la maîtrise des 
+                        transferts des compétences d’un domaine à un autre.</div>
+                    </div>
+                    <div class = {styles.second}>
+                        <ProgrammeItems />
+                    </div>
+                    <div class = {styles.third}></div>
+                </div>
+            </Container>
+            <CourbeOut class = {styles.courbe_out} />
         </section>
+    )
+}
+
+function CourbeIn(props){
+    return(
+        <svg viewBox="0 0 677 129" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+            <path d="M-493.686 55.1516C-493.686 55.1516 11.5429 134.634 199.81 57.5409C388.077 -19.5522 514.675 -193.109 568.7 -345.167C621.542 -493.9 639.324 -816.464 639.324 -816.464" stroke="#7B24EA" stroke-width="75"/>
+        </svg>
+    )
+}
+
+function CourbeOut(props){
+    return(
+        <svg viewBox="0 0 1369 481" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+            <path d="M1446 38C1446 38 1127.93 84.0771 971.667 187.398C815.403 290.719 723.871 416.151 654.982 504.295C392.696 839.895 23 558.729 23 558.729" stroke="#7B24EA" stroke-width="75"/>
+        </svg>
     )
 }
---
+++ composants/programmeItems/programmeItem.jsx
@@ -0,0 +1,94 @@
+import { createSignal } from "solid-js"
+import styles from './programmeItem.module.css'
+
+const [openScene, setOpenScene] = createSignal(false)
+const [mousePositionOnComponent, setMousePositionOnComponent] = createSignal({
+    px: 0,
+    py: 0
+})
+
+
+const handleMouseOver = (e) => {
+    setOpenScene(!openScene())
+    if(!openScene()){
+        setMousePositionOnComponent({
+            px : 0,
+            py : 0
+        })
+    }
+}
+
+export default function ProgrammeItems(){
+
+    const trackerMousePositionInComponent = (e) => {
+        if(openScene()){
+             setMousePositionOnComponent({
+                 px : e.layerX * 0.2,
+                 py : e.layerY * 0.2
+             })
+        } 
+     }
+
+    return(
+        <aside class={styles.programmeItems} onMouseOver={handleMouseOver} onMouseOut={handleMouseOver} onMouseMove={trackerMousePositionInComponent} >
+            <BackgroundAside />
+            <ImageComponent />
+            <InfoComponent />
+        </aside>
+    )
+}
+
+
+const BackgroundAside = () => {
+    
+    
+    
+    
+    return(
+        <div class={styles.backgroundAside}>
+            <div>
+                <div class={styles.before} style={{transform : `translateX(-${mousePositionOnComponent().px}px) translateY(${mousePositionOnComponent().py}px)`}}></div>
+                {
+                    openScene()
+                    ? 'je suis au-dessus'
+                    : 'je suis sortie'
+                }
+                <div class={styles.after} style={{transform : `translateX(${mousePositionOnComponent().px}px) translateY(-${mousePositionOnComponent().py}px)`}}></div>
+            </div>
+        </div>
+    )
+}
+
+
+const ImageComponent = ({image}) => {
+    return(
+        <div class={styles.imageComponent}>
+            <img src='/entreprenariat-femme.jpg' alt="" />
+        </div>
+    )
+}
+
+const InfoComponent = () => {
+    return(
+        <div class={styles.infoComponent}>
+            <div>
+                
+                <div class={styles.desc}>
+                    <span>
+                        PPEF
+                        <span style={{width : '42px', height :'42px' , "background-color" : '#CDABF7', display : 'flex', "align-items" : 'center', "justify-content" : 'center', "border-radius" : '50%'}}>
+                            <FlecheGauche style ={{ color : '#5711B1', width : '24px', height :'24px', }} />
+                        </span>
+                    </span>
+                   <p class="p" style={{ "font-size" : '24px', "line-height" : 1, color : 'white'}}> Programme de Promotion de l’Entrepreneuriat Féminin...
+                        <span style={{color : '#CDABF7', display : 'block', 'padding-top' : '12px', "font-size" : '16px'}}> Avoir plus d'information </span>
+                   </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function FlecheGauche(props) {
+    return <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 32 32" {...props}><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="m12 26l10-10L12 6"/></svg>
+}
