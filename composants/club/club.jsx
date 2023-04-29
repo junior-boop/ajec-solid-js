e8b942284e8d315a512ac091ad79cba3f7587d05
Daniel Seppo Eke <juniorseppo3@gmail.com>
2023-04-29 12:36:20 +0200

    home-update

---
+++ composants/club/club.jsx
@@ -0,0 +1,36 @@
+import Container from '../container/container'
+import styles from './club.module.css'
+
+export function ClubSection(){
+    return(
+        <section>
+            <Container>
+                <div class="h1" style={{color : '#7b24ea', "text-align" : 'center'}}>
+                    Nos Differents Clubs
+                </div>
+                <div class={styles.grid_template_3}>
+                    <Club />
+                    <Club />
+                    <Club />
+                    <Club />
+                    <Club />
+                    <Club />
+                </div>
+            </Container>
+        </section>
+    )
+}
+
+
+function Club ({name, image, }){
+    return(
+        <div class={styles.club}>
+            <div class={styles.clubContainer}>
+                <div class={styles.img} style = {{"background-image" : `url(${image})`}}>
+                    
+                </div>
+                <div class="h4"></div>
+            </div>
+        </div>
+    )
+}
