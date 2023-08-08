07184e0eccfe36a3b9539d2dd4c5f66a52387ebd
junior-boop <54283084+junior-boop@users.noreply.github.com>
2023-08-08 15:49:04 +0100

    Add files via upload

--- src/clubpage/clubpages.jsx
+++ src/clubpage/clubpages.jsx
@@ -2,40 +2,74 @@ import Container from '../../composants/container/container'
 import Vide from '../../composants/vide'
 import styles from './clubpages.module.css'
 
-import image1  from '../../src/assets/laboratoire_white.png'
-import image2  from '../../src/assets/croissance_white.png'
-import image3  from '../../src/assets/changement-climatique_white.png'
-import image4  from '../../src/assets/maison-ecologique_white.png'
-import image5  from '../../src/assets/plumes_white.png'
-import image6  from '../../src/assets/sensible_white.png'
-import image1_bis  from '../../src/assets/laboratoire.png'
-import image2_bis  from '../../src/assets/croissance.png'
-import image3_bis  from '../../src/assets/changement-climatique.png'
-import image4_bis  from '../../src/assets/maison-ecologique.png'
-import image5_bis  from '../../src/assets/plumes.png'
-import image6_bis  from '../../src/assets/sensible.png'
+import image1 from '../../src/assets/laboratoire_white.png'
+import image2 from '../../src/assets/croissance_white.png'
+import image3 from '../../src/assets/changement-climatique_white.png'
+import image4 from '../../src/assets/maison-ecologique_white.png'
+import image5 from '../../src/assets/plumes_white.png'
+import image6 from '../../src/assets/sensible_white.png'
 
-import Button from '../../composants/boutton/boutton'
-import { A,  useLocation } from '@solidjs/router'
+import image1_bis from '../../src/assets/laboratoire.png'
+import image2_bis from '../../src/assets/croissance.png'
+import image3_bis from '../../src/assets/changement-climatique.png'
+import image4_bis from '../../src/assets/maison-ecologique.png'
+import image5_bis from '../../src/assets/plumes.png'
+import image6_bis from '../../src/assets/sensible.png'
+
+import imageTest from '../images/M_ATEBA_AMBA_Gabriel_Président-Fondateur_de_l_AJEC.jpg'
+import imageTest_2 from '../images/M_TCHOMOBE_Patrick_Expert_informaticien_Secrétaire_Général_de_l_AJEC.jpg'
+
+import { texte as club_a_a } from '../pages/clubs/club-a-a/texte.js'
+import { texte as club_ai_ic } from '../pages/clubs/club-ai-ic/texte.js'
+import { texte as club_e_cl } from '../pages/clubs/club-e-cl/texte.js'
+import { texte as club_er } from '../pages/clubs/club-er/texte.js'
+import { texte as club_jpr } from '../pages/clubs/club-jpr/texte.js'
+import { texte as club_n } from '../pages/clubs/club-n/texte.js'
+
+import { A } from '@solidjs/router'
+import SolidMarkdown from 'solid-markdown'
+import { createSignal } from 'solid-js'
+import Navigation from '../constante/navigation.state'
+import { responsable } from './reponsable'
 
 
+const [clubParm, setClubParms] = createSignal('')
 
-export default function ClubsPages(){
-    const {state} = useLocation()
-    console.log(state)
 
-    const { image, titre } = state
+export default function ClubsPages() {
+    const state = Navigation()
+    const { image, titre, content, pdf } = state
 
-    return(
+    const contenu = () => {
+        switch (content) {
+            case 'a-a':
+                return club_a_a;
+            case 'ai-ic':
+                return club_ai_ic;
+            case 'e-cl':
+                return club_e_cl;
+            case 'er':
+                return club_er;
+            case 'jpr':
+                return club_jpr;
+            case 'num':
+                return club_n;
+        }
+    }
+
+
+    return (
         <div class={styles.clubsPages}>
+            <title> AJEC | Club {titre}</title>
+
             <Vide />
-            <ClubHeader image={image} titre={titre} />
+            <ClubHeader image={image} titre={titre} pdf={pdf} />
             <div>
                 <Container>
                     <div class={styles.contenu}>
                         <div class={styles.left}>
-                            <Contenu />
-                            bonjour
+                            <Contenu contenu={contenu()} pdf={pdf} titre={titre} club={content} />
+
                         </div>
                         <div class={styles.right}>
                             <AsideBar />
@@ -48,97 +82,136 @@ export default function ClubsPages(){
 }
 
 
-function ClubHeader({ image, titre }) {
-    return(
+function ClubHeader({ image, titre, pdf = '/toujours.pdf' }) {
+    return (
         <header>
             <Container>
-               <div class={styles.ClubHeader}>
+                <div class={styles.ClubHeader}>
                     <div class={styles.clubstitrebox}>
                         <div class={styles.clubicon}>
-                            <div style={{"background-image" : `url(${image})`}}></div>
+                            <div style={{ "background-image": `url(${image})` }}></div>
                         </div>
                         <div class={styles.clubstitle}>
                             <div className="p">CLUB ENTREPRENEURIAL</div>
                             <div class='h2'>{titre}</div>
-                            <A href='/toujours.pdf'>
+                            <A href={pdf} download={titre}>
                                 <button>
                                     Télécharger le PDF
                                 </button>
                             </A>
                         </div>
                     </div>
-               </div>
+                </div>
             </Container>
         </header>
     )
 }
 
-function AsideBar(){
-    return(
-        <div class={styles.asidebar}> 
+function AsideBar() {
+    return (
+        <div class={styles.asidebar}>
             <div>
-                <div className="h4" style={{ "font-size" : '24px', "font-weight" : '600', "line-height" : 1.1}}>
+                <div className="h4" style={{ "font-size": '24px', "font-weight": '600', "line-height": 1.1 }}>
                     Les différents clubs Entrepreneurial
                 </div>
             </div>
             <ul>
                 <li>
-                    <Link href='/clubs/ai-ic' onClick={() => {}} state={{ image : image1, titre : "Club d'Entrepreneuriat Agro-industrie et Industrie Chimique (AI/IC)" }}>
-                        <span class={styles.icon} style={{"background-image" : `url(${image1_bis})`}}></span> 
-                        <span>Club d'Entrepreneuriat Agro-industrie et Industrie Chimique (AI/IC)</span> 
+                    <Link href='/clubs/ai-ic' state={{ image: image1, titre: "Club d'Entrepreneuriat Agro-industrie et Industrie Chimique (AI/IC)", content: 'ai-ic' }}>
+                        <span class={styles.icon} style={{ "background-image": `url(${image1_bis})` }}></span>
+                        <span>Club d'Entrepreneuriat Agro-industrie et Industrie Chimique (AI/IC)</span>
                     </Link>
                 </li>
                 <li>
-                    <Link href='/'>
-                        <span class={styles.icon} style={{"background-image" : `url(${image2_bis})`}}></span> 
-                        <span>Club d'Entrepreneuriat Agriculture et Agroforesterie (A/A)</span> 
+                    <Link href='/clubs/a-a' state={{ image: image2, titre: "Club d'Entrepreneuriat Agro-industrie et Industrie Chimique (AI/IC)", content: 'ai-ic' }}>
+                        <span class={styles.icon} style={{ "background-image": `url(${image2_bis})` }}></span>
+                        <span>Club d'Entrepreneuriat Agriculture et Agroforesterie (A/A)</span>
                     </Link>
                 </li>
                 <li>
-                    <Link href='/'>
-                        <span class={styles.icon} style={{"background-image" : `url(${image3_bis})`}}></span> 
+                    <Link href='/clubs/e-cl'>
+                        <span class={styles.icon} style={{ "background-image": `url(${image3_bis})` }}></span>
                         <span> Environnement et Changements Climatiques (E/CL)</span>
                     </Link>
                 </li>
                 <li>
-                    <Link href='/'>
-                        <span class={styles.icon} style={{"background-image" : `url(${image4_bis})`}}></span> 
+                    <Link href='/clubs/er'>
+                        <span class={styles.icon} style={{ "background-image": `url(${image4_bis})` }}></span>
                         <span> Club d'Entrepreneuriat Energies Renouvelables (ER)</span>
                     </Link>
                 </li>
                 <li>
-                    <Link href='/'>
-                        <span class={styles.icon} style={{"background-image" : `url(${image5_bis})`}}></span> 
+                    <Link href='/clubs/jpr'>
+                        <span class={styles.icon} style={{ "background-image": `url(${image5_bis})` }}></span>
                         <span>Club d'Entrepreneuriat Journal-Poésie-Roman (JPR)</span>
                     </Link>
                 </li>
                 <li>
-                    <Link href='/'>
-                        <span class={styles.icon} style={{"background-image" : `url(${image6_bis})`}}></span> 
+                    <Link href='/clubs/num'>
+                        <span class={styles.icon} style={{ "background-image": `url(${image6_bis})` }}></span>
                         <span>Club d'Entrepreneuriat Numérique (N)</span>
-                    
+
                     </Link>
                 </li>
-                
+
             </ul>
         </div>
     )
 }
 
 
-function Link({href = "/", children, onClick, state}){
-    return(
-        <A href={href} onClick = {onClick} state={state}>
+function Link({ href = "/", children }) {
+    return (
+        <A href={href} onclick={() => setTimeout(() => location.reload(), 300)}>
             {children}
         </A>
     )
 }
 
-function Contenu(){
-    return(
+function Contenu({ contenu, pdf, titre, club }) {
+
+    const respo = responsable
+    const trie = () => {
+        const respoClub = respo.filter(el => el.club === club)
+        return respoClub;
+    }
+
+    console.log(trie())
+    return (
         <div class={styles.leftContenu}>
-            bonjour
-            <h1>ville </h1>
+
+            <SolidMarkdown children={contenu} />
+            <div style={{ padding: '2rem', "background-color": '#F7F1FE', "margin-top": '2rem' }} >
+                Si vous souhaitez avoir plus d'information sur ce club et son programme, <A href={pdf} download={titre}>cliquez ici pour télécharger le PDF</A>
+            </div>
+            <div style={{ margin: '5rem 0' }}>
+                <h1>Membre de l'équipe technique</h1>
+                <div class={styles.memberContent}>
+                    {
+                        trie().map(el => (<Member
+                            image={''}
+                            nom={el.nom}
+                            poste={el.poste}
+                            job={el.job}
+                            description={el.description}
+                        />))
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export function Member({ image, nom, poste, job, description }) {
+    return (
+        <div class={styles.member}>
+            <div class={styles.memberImage} style={{ "background-image": `url(${image})` }}></div>
+            <div class={styles.infos}>
+                <div class='h4' style={{ "line-height": 1.2, "margin-bottom": '.7rem' }}>{nom}</div>
+                <div style={{ "font-size": '1.15rem', "font-weight": '600', "line-height": 1.2, "margin-bottom" : '.5rem' }}>{poste}</div>
+                <div style={{ "font-size": '1rem', "font-weight": '600', "line-height": 1.2, "margin-bottom": '.6rem', "font-style":"italic" }}>{job}</div>
+                <div>{description}</div>
+            </div>
         </div>
     )
 }
---
+++ src/clubpage/reponsable.js
@@ -0,0 +1,190 @@
+import ateba from '../images/M_ATEBA_AMBA_Gabriel_Président-Fondateur_de_l_AJEC.jpg'
+import patrick from '../images/M_TCHOMOBE_Patrick_Expert_informaticien_Secrétaire_Général_de_l_AJEC.jpg'
+import dongmo from '../images/M_DONGMO_Apollinaire_PLEG_Membre.jpg'
+import ndam from '../images/Dr_NDAM_Youchahou_Secrétaire_PhD_UY1.jpg'
+import etoa from '../images/M_ETOA_Yves_Coordonnateur_National_Adj._PLEG _SVT_Inspecteur_Pédagogique_Régional.jpg'
+import meledjie from '../images/M_MELEDJIE_Epifani_Experte_en_Gestion_Administrative_et_Financière_Membre.jpg'
+import mohamed from '../images/M_Mohamed_POKASSA_Ingénieur_de_Conception_des_Travaux_Agronomiques-Production_Végétale_Membre.jpg'
+import ngoufo from '../images/M_NGOUFO_Hyacinthe_PLEG_Membre.jpg'
+import nkoa from '../images/M_NKOA_ETEME_Luc_Ingénieur_des_Travaux,_Environnement_et_Développement_Durable,_Membre.jpg'
+import tchulang from '../images/M_TCHULANG_K._Triomphant_Ingénieur_de_Conception_en_Energies_Renouvelables,_Membre.jpg'
+import venus from '../images/M_Venus_PLOTON_Diplômée_de_l_Ecole_des_Hautes_Etudes_en_Santé_Publique,_Membre.jpg'
+import calista from '../images/Mme_Calista_AYUK_Experte_Interprète_et_Traductrice,_Membre.jpg'
+import ayissi from '../images/Prof._AYISSI_MBOMO_Rigobert_Espoir,_Coordonateur_National.jpg'
+
+
+export const responsable = [
+    {
+        nom : "M.ATEBA AMBA GABRIEL",
+        poste : "Président de l’AJEC",
+        job : "atebaamba@yahoo.com",
+        description : "spécialiste en question Formation-Emploi-Entreprenariat ; Chef service de l’entrepreneuriat, projets et leadership (SEPL)",
+    },
+    {
+        nom : "M. ASSOMO Péguy",
+        poste : "Premier Vice-Président de l’AJEC",
+        job : "assomo_ndemba@gmail.com",
+    },
+    {
+        nom : "M.TCHULANG KOUOGUE Triomphant",
+        poste : "Deuxième Vice- Président de l’AJEC",
+        job : "Ingénieur de Conception en énergies renouvelables",
+        description : "Responsable Numero 2  du Club d’Entrepreneuriat Energies Renénouvelables (ER)",
+    },
+    {
+        nom : "M.TCHOMOBE PATRICK ",
+        poste : "Secrétaire Général de l’AJEC",
+        job : "Expert informaticien",
+        description : "Chef service des affaires administratives et financières (SAAF)",
+        club : ""
+    },
+    {
+        nom : "NGONGOLA Vicky Christelle",
+        poste : "Secrétaire Général adjoint de l’AJEC",
+        job : "ngonngola@christelle@gmail.com",
+        description : "Chef service des affaires administratives et financières (SAAF)",
+    },
+    {
+        nom : "M. MELEDJIE Epifani",
+        poste : "Censeur",
+        job : "Epifanimeledjie12@gmail.com",
+    },
+    {
+        nom : "NJANKOUO NDAM Youchahou",
+        poste : "Resp. Labo Conception Développement des Projets",
+        job : "ndamyouchahou@yahoo.fr",
+    },
+    {
+        nom : "Dr. SOP Berthelot",
+        poste : "Resp. Labo Conception Développement des Projets",
+        job : "berthelot_sop@yahoo.fr",
+    },
+    {
+        nom : "M. ELAM Jean Christian",
+        poste : "Resp. de la Communication et de la Coopération",
+        job : "christianelam@gmail.com",
+    },
+    {
+        nom : "ABAH MFOMO Joseph Franky ",
+        poste : "Resp. de la Communication et de la Coopération",
+        job : "abahmfomojosephfranky@gmail.com",
+    },
+    {
+        nom : "NGOUFO Hyacinthe",
+        poste : "Resp. Affaires académiques",
+        job : "h.ngoufo@yahoo.com",
+        description : "Responsables du Club d’Entrepreneuriat de l’Environnement et Changements Climatiques (E/CL)",
+        club : "e-cl"
+    },
+    {
+        nom : "M.EKOBENA EKOBENA Pierre",
+        poste : "conseiller",
+        job : "ekoberfr@yahoo.fr",
+    },
+    {
+        nom : "M.DONGMO Apollinaire",
+        poste : "conseiller",
+        job : "ekoberfr@yahoo.fr",
+    },
+    {
+        nom : "MISSE BEBE Ingrid ",
+        poste : "Conseiller",
+        job : "",
+    },
+    {
+        nom : "M. ETOA Yves ",
+        poste : "Conseiller",
+        job : "yvestoa2019@gmail.com",
+    },
+    
+    {
+        nom : "M.OKOUDA ATEBA ALEXANDRE",
+        poste : "Trésorier adjoint de l’AJEC",
+        job : "Ingénieur Agroindustriel ",
+        description : "Chargé n02 du Club d’Entrepreneuriat Agroindustrie et Industrie chimique (AI/IC)",
+    },
+    {
+        nom : "M. OMGBA GUY ZACHARIE",
+        poste : "Commissaire aux Comptes de l’AJEC",
+        job : "Expert-comptable ",
+        description : "Chef de la Cellule de la Comptabilité et des Finances à l’AJEC",
+        club : ""
+    },
+    {
+        nom : "Mme DASSI ",
+        poste : "trésorière de l’AJEC",
+        job : "Entrepreneur",
+        description : "",
+        club : ""
+    },
+    {
+        nom : "M. MOHAMA ",
+        poste : "Represant de l’AJEC à l’extérieur",
+        job : "Expert en communication",
+        description : "",
+        club : ""
+    }
+]
+
+export const Scientifique = [
+    {
+        nom : "Pr AYISSI MBOMO Rigobert Espoir",
+        poste : "Coordonnateur National",
+        job : "Professeur des Universités, Enseignant à l’ENS Yaoundé",
+        description : "espybass@yahoo.fr"
+    },
+    {
+        nom : "ETOA Yves",
+        poste : "Coordonnateur National Adj.",
+        job : "PLEG, Inspecteur Pédagogique ",
+        description : "yvestoa2019@gmail.com"
+    },
+    {
+        nom : "Dr NJANKOUO NDAM Y.",
+        poste : "Secrétaire",
+        job : "Docteur PhD en Biochimie Université de Yaoundé 1 ",
+        description : "ndamyouchahou@yahoo.fr"
+    },
+    {
+        nom : "OKOUDA ATEBA Alexandre ",
+        poste : "Rapporteur",
+        job : "Ingénieur en Sciences et Technologies Alimentaires",
+        description: "laxetbois@gmail.com"
+    },
+    {
+        nom : "NGO MOUNE Frida Flore",
+        poste : "Membre",
+        job : "Experte en Science de l’Education",
+        description : "floremoune@gmail.com"
+    },
+    {
+        nom : "Dr SOP Berthelot ",
+        job : "Ph. D Chargé de cours à l’Université de Ngaoundéré",
+        poste : "Resp. Labo Conception Développement des Projets",
+        description : "berthelot_sop@yahoo.fr"
+    },
+    {
+        nom : "AMBOMO Levis Williams ",
+        job : "",
+        poste : "Membre",
+        description : "leviswilliamsambomo@gmail.com"
+    },
+    {
+        nom : "ABAH MFOMO Franky ",
+        poste : "Membre",
+        job : "Expert en Communication et Information Documentaire. ESSTIC",
+        description : "abahmfomojosephfranky@gmail.com"
+    },
+    {
+        nom : "POKASSA MFEUJOU MOHAMED Moctar",
+        poste : "Membre",
+        job : "Ingénieur de Conception des Travaux Agronomiques, Production Végétale ",
+        description : "mpokassamfeujou@gmail.com"
+    },
+    {
+        nom : "TCHULANG K. Triomphant",
+        poste : "Membre",
+        job : "Ingénieur de Conception en Energies Renouvelables",
+        description : "triomphant@cleanenergyservices.com"
+    }
+]
