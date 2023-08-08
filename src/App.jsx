e95701addbb1c8f0c56f3c96d645752cb8de86d6
junior-boop <54283084+junior-boop@users.noreply.github.com>
2023-08-08 15:41:28 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,30 @@ import Home from './home/home';
 import NavBar from '../composants/navigationBar/navBar';
 import Footer from '../composants/footer/footer';
 import ClubsPages from './clubpage/clubpages';
+import Apropos from './Apropos/apropos';
+import Activity from '../composants/activites/activites';
+import ActivityPages from '../composants/activites/pageactivites';
+import ProgrammmePage from './programme';
+import ProgrammePPEF from './programme/ppef-icelc';
+import Galerie from './galerie';
 
-function useData({params, location, data, navigate}){
-  console.log(params, location, data, navigate)
-}
 
 
 function App() {
+  console.log('je suis la vie')
   return (
     <>
     <NavBar />
       <Routes>
         <Route path="/" component ={Home} />
-        <Route path="/clubs">
-          <Route path='/:id' component={ClubsPages} data={useData} />
-        </Route>
+        <Route path="/activites" component ={Activity} />
+        <Route path="/activites/:id" component ={ActivityPages} />
+        <Route path="/devenir-membre" component ={Home} />
+        <Route path="/a-propos" component ={Apropos} />
+        <Route path = "/programme" component={ProgrammmePage} />
+        <Route path="/programme/ppef-icelc" component ={ProgrammePPEF} />
+        <Route path='/clubs/:id' component={ClubsPages} />
+        <Route path='/galerie/:id' component={Galerie} />
       </Routes>
     <Footer />
     </>
